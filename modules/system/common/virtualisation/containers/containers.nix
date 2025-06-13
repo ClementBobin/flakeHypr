c195606d4842ea0efc0d679b62fedfac32d66ef6
mirage <119869686+ClementBobin@users.noreply.github.com>
2025-06-13 15:52:23 +0200

    refactor: :art: streamline container engine configuration in nix

--- modules/system/common/virtualisation/containers/containers.nix
+++ modules/system/common/virtualisation/containers/containers.nix
@@ -7,26 +7,25 @@
 }:
 
 let
-  cfg = config.modules.virtualisation;
+  cfg = config.modules.virtualisation.containers;
 in
 {
-  options.modules.virtualisation = {
-    docker = {
-      enable = lib.mkEnableOption "Enable Docker support";
-    };
-    podman = {
-      enable = lib.mkEnableOption "Enable podman";
+  options.modules.virtualisation.containers = {
+    engine = lib.mkOption {
+      type = lib.types.listOf (lib.types.enum ["docker" "podman"]);
+      default = [];
+      description = "List of container engines to enable";
     };
   };
   config = lib.mkMerge [
-    (lib.mkIf cfg.docker.enable {
+    (lib.mkIf (lib.elem "docker" cfg.engine) {
       users.groups.docker.members = [ "${vars.user}" ];
 
       virtualisation.docker.enable = true;
 
       environment.systemPackages = [ pkgs.docker pkgs.docker-compose ];
     })
-    (lib.mkIf cfg.podman.enable {
+    (lib.mkIf (lib.elem "podman" cfg.engine) {
       virtualisation.podman.enable = true;
 
       environment.systemPackages = [ pkgs.podman pkgs.podman-compose ];
