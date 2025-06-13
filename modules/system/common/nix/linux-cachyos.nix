97e3304e2426d2f526ad9b9fa1956914234613d1
mirage <119869686+ClementBobin@users.noreply.github.com>
2025-06-13 16:38:11 +0200

    feat: :sparkles: add option to conditionally enable scx service

--- modules/system/common/nix/linux-cachyos.nix
+++ modules/system/common/nix/linux-cachyos.nix
@@ -13,12 +13,13 @@ in
   imports = [ inputs.chaotic.nixosModules.default ];
   options.modules.nix.linux-cachyos = {
     enable = lib.mkEnableOption "Enable Chaotic-AUR (CachyOS) support";
+    enableSCX = lib.mkEnableOption "Enable SCX service when using Chaotic-AUR (CachyOS)";
   };
 
   config = lib.mkIf cfg.enable {
 
     boot.kernelPackages = lib.mkForce pkgs.linuxPackages_cachyos;
-    services.scx.enable = true;
+
     nix.settings = {
       substituters = [
         "https://chaotic-nyx.cachix.org"
@@ -27,5 +28,8 @@ in
         "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
       ];
     };
+
+    # Conditionally enable services.scx
+    services.scx.enable = lib.mkIf cfg.enableSCX true;
   };
 }
