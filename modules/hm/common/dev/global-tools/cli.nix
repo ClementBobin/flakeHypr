2240ca9d1e754e8dbeee40edde74bd5f7123897a
mirage <119869686+ClementBobin@users.noreply.github.com>
2025-06-13 16:52:30 +0200

    fix: :bug: refactor home.packages to use concatenation

--- modules/hm/common/dev/global-tools/cli.nix
+++ modules/hm/common/dev/global-tools/cli.nix
@@ -14,9 +14,9 @@ in
   };
 
   config = {
-    home.packages = (with pkgs; [
-      (lib.optionals (lib.elem "vercel" cfg.elements) nodePackages.vercel)
-      (lib.optionals (lib.elem "graphite" cfg.elements) graphite-cli)
-    ]);
+    home.packages = (with pkgs;
+      lib.optional (lib.elem "vercel" cfg.elements) nodePackages.vercel ++
+      lib.optional (lib.elem "graphite" cfg.elements) graphite-cli
+    );
   };
 }
