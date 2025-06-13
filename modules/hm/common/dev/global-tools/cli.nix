b83538a22738fab3ef795513817826bbfafb5b7d
mirage <119869686+ClementBobin@users.noreply.github.com>
2025-06-13 10:40:57 +0200

    fix: :bug: update cli configuration to reflect global tools structure

--- modules/hm/common/dev/global-tools/cli.nix
+++ modules/hm/common/dev/global-tools/cli.nix
@@ -1,16 +1,16 @@
 { pkgs, lib, config, ... }:
 
 let
-  cfg = config.modules.common.dev.node.cli;
+  cfg = config.modules.common.dev.global-tools.cli;
 in
 {
-  options.modules.common.dev.node.cli = {
-    enable = lib.mkEnableOption "Enable CLI for Node.js deployments";
+  options.modules.common.dev.global-tools.cli = {
+    enable = lib.mkEnableOption "Enable CLI";
 
     elements = lib.mkOption {
       type = lib.types.listOf (lib.types.enum ["vercel" "graphite"]);
       default = ["vercel"];
-      description = "List of CLI tools to install for Node.js deployments";
+      description = "List of CLI tools to install";
     };
   };
 
