02558e069b90256ad0e2fada884b4253c5c0c71f
mirage <119869686+ClementBobin@users.noreply.github.com>
2025-06-13 17:33:50 +0200

    fix: :bug: refactor packages configuration for improved driver handling

--- modules/hm/common/browser/default.nix
+++ modules/hm/common/browser/default.nix
@@ -18,17 +18,21 @@ in
   };
 
   config = {
-    home.packages = with pkgs; (
-      (lib.optionals (lib.elem "chromium" cfg.emulators) [chromium]) ++
-      (lib.optionals (lib.elem "chrome" cfg.emulators) [google-chrome]) ++
-      (lib.optionals (lib.elem "firefox" cfg.emulators) [firefox]) ++
-      (lib.optionals (lib.elem "brave" cfg.emulators) [brave]) ++
-      (lib.optionals (lib.elem "vivaldi" cfg.emulators) [vivaldi]) ++
-      (lib.optionals (lib.elem "edge" cfg.emulators) [microsoft-edge]) ++
-
-      (lib.optionals (cfg.driver.enable && (lib.elem "chromium" cfg.emulators || lib.elem "chrome" cfg.emulators || lib.elem "vivaldi" cfg.emulators || lib.elem "brave" cfg.emulators)) [chromedriver]) ++
-      (lib.optionals (cfg.driver.enable && lib.elem "firefox" cfg.emulators) [geckodriver]) ++
-      (lib.optionals (cfg.driver.enable && lib.elem "edge" cfg.emulators) [msedgedriver])
-    );
+    home.packages = with pkgs;
+      lib.unique (
+        (lib.optionals (lib.elem "chromium" cfg.emulators) [chromium]) ++
+        (lib.optionals (lib.elem "chrome" cfg.emulators) [google-chrome]) ++
+        (lib.optionals (lib.elem "firefox" cfg.emulators) [firefox]) ++
+        (lib.optionals (lib.elem "brave" cfg.emulators) [brave]) ++
+        (lib.optionals (lib.elem "vivaldi" cfg.emulators) [vivaldi]) ++
+        (lib.optionals (lib.elem "edge" cfg.emulators) [microsoft-edge]) ++
+        (lib.optionals
+          (cfg.driver.enable &&
+            (lib.any (browser: lib.elem browser cfg.emulators)
+                     [ "chromium" "chrome" "vivaldi" "brave" ]))
+          [chromedriver]) ++
+        (lib.optionals (cfg.driver.enable && lib.elem "firefox" cfg.emulators) [geckodriver]) ++
+        (lib.optionals (cfg.driver.enable && lib.elem "edge" cfg.emulators) [msedgedriver])
+      );
   };
 }
