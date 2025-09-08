7428752c21a5dee459c67a39d0a338a4aab436b8
mirage <119869686+ClementBobin@users.noreply.github.com>
2025-09-08 19:29:23 +0200

    fix: refactor shell tools configuration to use default options for better clarity and maintainability

--- modules/hm/common/shell/tools.nix
+++ modules/hm/common/shell/tools.nix
@@ -6,42 +6,50 @@ in
 {
   options.modules.hm.shell.tools = {
     enable = lib.mkEnableOption "Enable common shell tools";
+    shellAliases = lib.mkOption {
+      type = lib.types.attrsOf lib.types.str;
+      default = {};
+      description = "Shell aliases to set up";
+      example = { ll = "ls -la"; gs = "git status"; };
+    };
   };
 
   config = lib.mkIf cfg.enable {
     # Install shell tools via home-manager
-    home.packages = (with pkgs; [
-      tree
-    ]);
+    home = {
+      packages = (with pkgs; [
+        tree
+      ]);
+      shellAliases = cfg.shellAliases;
+    };
 
     programs = {
-      ranger.enable = true;
+      ranger.enable = lib.mkDefault true;
       navi = {
         # Enable navi
-        enable = true;
-        enableZshIntegration = true;
+        enable = lib.mkDefault true;
+        enableZshIntegration = lib.mkDefault true;
       };
       fzf = {
-        enable = true;
-        enableZshIntegration = true;
-        tmux.enableShellIntegration = true;
+        enable = lib.mkDefault true;
+        enableZshIntegration = lib.mkDefault true;
       };
       btop = {
 
         # Enable btop
-        enable = true;
+        enable = lib.mkDefault true;
 
         # Configuration for btop
         settings = {
 
           # Use default terminal background
-          theme_background = false;
+          theme_background = lib.mkDefault false;
 
           # Use vim keys
-          vim_keys = true;
+          vim_keys = lib.mkDefault true;
 
           # Organise processes as a tree by default
-          proc_tree = true;
+          proc_tree = lib.mkDefault true;
         };
       };
     };
