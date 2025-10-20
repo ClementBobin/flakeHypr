498d2e9bcc985c1ba381cc5da113fd7c3e8539a3
mirage <119869686+ClementBobin@users.noreply.github.com>
2025-10-20 22:42:08 +0200

    feat: refactor Android configuration by removing Flutter support and streamlining environment setup

--- modules/system/common/dev/languages/flutter.nix
+++ modules/system/common/dev/languages/flutter.nix
@@ -1,68 +1,23 @@
 { pkgs, lib, config, vars, ... }:
 
 let
-  cfg = config.modules.system.dev.languages.flutter;
-
-  # Android SDK configuration
-  androidEnv = pkgs.androidenv.override { licenseAccepted = true; };
-  androidComposition = androidEnv.composeAndroidPackages {
-    buildToolsVersions = cfg.android.buildToolsVersions;
-    platformVersions = cfg.android.platformVersions;
-    abiVersions = cfg.android.abiVersions;
-  };
-  androidSdk = androidComposition.androidsdk;
+  cfg = config.modules.system.dev.languages.android;
 in
 {
-  options.modules.system.dev.languages.flutter = {
-    enable = lib.mkEnableOption "Flutter development environment";
-
-    withAndroid = lib.mkEnableOption "Include Android SDK tooling";
-
-    jdkPackage = lib.mkOption {
-      type = lib.types.package;
-      default = pkgs.jdk17;
-      description = "Java Development Kit package to use for Flutter development";
-    };
-
-    android = {
-      buildToolsVersions = lib.mkOption {
-        type = lib.types.listOf lib.types.str;
-        default = [ "34.0.0" "28.0.3" ];
-        description = "List of Android build tools versions to include.";
-      };
-
-      platformVersions = lib.mkOption {
-        type = lib.types.listOf lib.types.str;
-        default = [ "34" "28" ];
-        description = "List of Android platform versions to include.";
-      };
-
-      abiVersions = lib.mkOption {
-        type = lib.types.listOf lib.types.str;
-        default = [ "armeabi-v7a" "arm64-v8a" ];
-        description = "List of Android ABI versions to include.";
-      };
-    };
+  options.modules.system.dev.languages.android = {
+    enable = lib.mkEnableOption "Android development environment";
   };
 
   config = lib.mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      android-studio
+    ];
 
-    environment.systemPackages =
-      [
-        pkgs.flutter
-        cfg.jdkPackage
-      ]
-      ++ lib.optional cfg.withAndroid androidSdk;
-
-    # Environment variables
-    environment.variables = lib.mkIf cfg.withAndroid {
-      ANDROID_HOME = "${androidSdk}/libexec/android-sdk";
-    };
-
-    programs = lib.mkIf cfg.withAndroid { adb.enable = true; };
+    programs.adb.enable = true;
 
     users.users.${vars.user}.extraGroups = [
       "adbusers"
+      "kvm"
     ];
   };
 }
