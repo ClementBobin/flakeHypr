eb48a7759362a651d23d882e30bbc67ef54c8762
Mirage <119869686+ClementBobin@users.noreply.github.com>
2025-10-22 16:29:08 +0200

    Refactor API configuration to support multiple clients

--- modules/hm/common/utilities/api.nix
+++ modules/hm/common/utilities/api.nix
@@ -1,19 +1,32 @@
-{ config, lib, pkgs, ... }:
-
-with lib;
+{ pkgs, lib, config, ... }:
 
 let
   cfg = config.modules.hm.utilities.api;
-  scalarApp = (import ../../../wrapper/scalar.nix { inherit pkgs lib config; }).scalarApp;
+
+  # Map client to their packages
+  clientToPackage = {
+    scalar = (import ../../../wrapper/scalar.nix { inherit pkgs lib config; }).scalarApp;
+    yaak   = pkgs.yaak;
+  };
+
+  # Get packages for enabled clients
+  clientPackages = lib.filter (pkg: pkg != null)
+    (map (client: clientToPackage.${client} or null) cfg.clients);
+
 in {
   options.modules.hm.utilities.api = {
-    scalar.enable = mkEnableOption "Scalar desktop application";
+    clients = lib.mkOption {
+      type = lib.types.listOf (lib.types.enum (lib.attrNames clientToPackage));
+      default = [];
+      description = ''
+        Select which utilities (clients API) to install on your system.
+        You can choose one or multiple clients.
+      '';
+    };
   };
 
   config = {
-    home.packages = [
-      (lib.mkIf cfg.scalar.enable scalarApp)
-    ];
+    home.packages = clientPackages;
     xdg.enable = true;
   };
 }
